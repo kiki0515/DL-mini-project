e97a8d79d4039a8aeaddb4bfb25d90267ad7090d
jh8011nyu <jh8011@nyu.edu>
2022-11-17 18:41:27 -0500

    init commit

---
+++ config.py
@@ -0,0 +1,31 @@
+def get_configs():
+    train_params = {
+        "device": "cuda",
+        "max_epoch": 100,
+        "output_dir": "output"
+    }
+
+    dataset_params = {
+        "train_bs": 128,
+        "test_bs": 256,
+    }
+
+    model_params = {
+
+    }
+
+    callback_params = {
+        "patience": 3,
+        "save_final_model": True,
+    }
+
+    optimizer_params = {
+        "lr": 0.01,
+        "type": "AdamW",
+        "kwargs": {
+            "weight_decay": 0.1,
+            "amsgrad": True
+        }
+    }
+
+    return train_params, dataset_params, callback_params, optimizer_params
---
+++ utils/utils.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+
+def get_cifar10(train_bs=16, test_bs=128, transform_train=None, transform_test=None):
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=train_bs, shuffle=True)
+
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=test_bs, shuffle=False)
+
+    return trainloader, testloader
+
